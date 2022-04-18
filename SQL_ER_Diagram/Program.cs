08d5f11a8a9a1c661cfbaa76bfc4c7a7c9a4d425
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-04-18 10:19:26 +0530

    [Akanksha]: added ER diagram

---
+++ SQL_ER_Diagram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQL_ER_Diagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ER Diagram");
+        }
+    }
+}
